f31233a983ef14d76c67baee003d5bbe039856ae
karmacoma <karma@coma.lol>
2025-08-06 16:58:06 -0700

    first draft

---
+++ src/fullmoon.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract fullmoon {
+    int private constant b = 44;
+
+    function northern() public view returns (string memory) {
+        int a = 2551443;
+        int x = -b;
+        int y = 2 - b;
+        int z = (((int(block.timestamp) - 592531) % a) << 9) / a;
+
+        // output buffer, index and character to push
+        bytes memory o = new bytes(1024);
+        uint i = 0;
+        bytes1 c;
+
+        // pixels
+        bytes memory p = "#.";
+
+        while (y <= b) {
+            if (++x >= a) {
+                x = -b;
+                y += 4;
+                c = '\n';
+            } else {
+                if (x < 0) {
+                    if (x * x + y * y < b*b) {
+                        a = 1 - x;
+                        x = -1;
+                    } else {
+                        x++;
+                    }
+                    c = ' ';
+                } else {
+                    c = p[(x < a * (~z & 255) >> 8 ? 1 : 0) ^ uint(z >> 8)];
+                }
+            }
+            o[i++] = c;
+        }
+
+        return string(o);
+    }
+}
---
+++ test/moon.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {LibString} from "solady/utils/LibString.sol";
+
+import {moon} from "src/moon.sol";
+import {fullmoon} from "src/fullmoon.sol";
+
+// full moon: 1754689746
+// last quarter: 1755337746
+// new moon: 1755971346
+// first quarter: 1756601346
+contract MoonTest is Test {
+    moon public m;
+    fullmoon public f;
+
+    function setUp() public {
+        m = new moon();
+        f = new fullmoon();
+    }
+
+    function test_fullmoon_currenttime() public {
+        // use this to avoid the extra spaces at the beginning or between args
+        vm.warp(vm.unixTime() / 1000);
+        console.log("\n%s", f.northern());
+    }
+
+    function test_fullmoon_fullmoon() public {
+        vm.warp(1754689746);
+        console.log("\n%s", f.northern());
+    }
+
+    function test_fullmoon_lastquarter() public {
+        vm.warp(1755337746);
+        console.log("\n%s", f.northern());
+    }
+
+    function test_fullmoon_newmoon() public {
+        vm.warp(1755971346);
+        console.log("\n%s", f.northern());
+    }
+
+    function test_fullmoon_firstquarter() public {
+        vm.warp(1756601346);
+        console.log("\n%s", f.northern());
+    }
+
+    function test_moon_northern() public view {
+        string memory mb = m.northern();
+        string memory fb = f.northern();
+
+        assertEq(mb, fb);
+    }
+}
