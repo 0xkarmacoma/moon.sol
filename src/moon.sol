53299f7a4938b589e911efd08165eda476eef836
karmacoma <karma@coma.lol>
2025-08-08 15:07:40 -0700

    add src/noom.sol

--- src/moon.sol
+++ src/moon.sol
@@ -5,16 +5,18 @@ import "./noom.sol";
 
 contract moon is noom {
     function n(int ts) public pure override returns (bytes memory o) {
+
             o=new bytes(s);
-        int a=2551443;int   x=
-      -b;int y=2-b;int z =    (((
-     int(ts)-592531)%a)<<9)     /a;
-     uint i;while(y<=b){(a,x     ,y
-     ,o[i++])=++x>=a?(a,-b,y      +
-     4,l):(x<0?(x*x+y*y<b*b)      ?
-     (1-x,-1,y,d):(a,x+1,y,      d
-      ):(a,x,y,p[(x<a*(~z &   255
-        ) >> 8 ? 1 : 0)^   uint
-            (z >> 8)])) ;}
+        int a=2551443;int  x=
+      -b;int y=2-b;int z=(    ((
+     int(ts)-592531)%a)<<9)    /a;
+     uint i;while(y<=b){(a,x    ,y
+     ,o[i++])=++x>=a?(a,-b,y     +
+     4,l):(x<0?(x*x+y*y<b*b)     ?
+     (1-x,-1,y,d):(a,x+1,y,     d
+      ):(a,x,y, p[(x<a*(~z    &
+        255) >> 8 ? 1 : 0   )^
+            uint(z>>8)]));}
+
     }
 }
---
+++ src/noom.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+abstract contract noom {
+    int constant b = 44;
+    uint constant s = 1024;
+    bytes constant p = "#.";
+    bytes1 constant d = bytes1(' ');
+    bytes1 constant l = bytes1('\n');
+
+    fallback(bytes calldata data) external returns (bytes memory) {
+        int timestamp = int(block.timestamp);
+        if (data.length != 0) {
+            timestamp = abi.decode(data, (int));
+        }
+        return n(timestamp);
+    }
+
+    function n() public view returns (bytes memory o) {
+        return n(int(block.timestamp));
+    }
+
+    function n(int ts) public pure virtual returns (bytes memory o);
+}
